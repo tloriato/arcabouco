51f1eff67dd90c476b7c71696ef6a2b358d347f1
Thiago Naves <naves.thiago@gmail.com>
2015-09-28 17:01:41 -0300

    Atualizado histórico

--- gamao/testes/lista/testlis.c
+++ gamao/testes/lista/testlis.c
@@ -10,8 +10,9 @@
 *           tdn - Thiago Duarte Naves
 *
 *  $HA Histórico de evolução:
-*     Versão        Autor              Data           Observações
-*     1.00          gbo, gapm, tdn     28/08/2015     Início do desenvolvimento.
+*     Versão    Autor              Data        Observações
+*     2.00      gbo, gapm, tdn     20/09/2015  Lista de ponteiros de void.
+*     1.00      gbo, gapm, tdn     28/08/2015  Início do desenvolvimento.
 *
 ***************************************************************************/
 
